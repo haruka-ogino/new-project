ea1180cdbb39e0c3d64d2751623eb08956360a7f
Haruka Ogino <harukariq@gmail.com>
2024-04-07 19:41:44 +1200

    new implementation used/displayed on Pets component

--- client/components/Pets.tsx
+++ client/components/Pets.tsx
@@ -8,6 +8,23 @@ export default function Pets() {
   return (
     <>
       <h2>PETS!</h2>
+      <ul>
+        <li>
+          {pet1.greet()}
+          <br />
+          {pet1.speak()}
+        </li>
+        <li>
+          {pet2.greet()}
+          <br />
+          {pet2.speak()}
+        </li>
+        <li>
+          {pet3.greet()}
+          <br />
+          {pet3.speak()}
+        </li>
+      </ul>
     </>
   )
 }
